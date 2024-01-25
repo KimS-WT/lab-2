c15364aaba42e2691a2613622bfed4138cff6eaa
Kimberlynn Sundblom <ksundblom1@buffs.wtamu.edu>
2024-01-25 15:15:54 -0600

    Final Commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using lab2_starter.Models;
+
 Widget widgetOne = new Widget();
 widgetOne.Name = "Microsoft";
 Console.WriteLine($"widgetOne.Name = {widgetOne.Name}");
+
 Widget widgetTwo = new Widget();
 widgetTwo.Name = "Samsung";
 Console.WriteLine($"widgetTwo.Name = {widgetTwo.Name}");
